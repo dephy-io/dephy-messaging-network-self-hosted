44a258bd1a098572c10452aeb90a460f09a92d7f
magine <magine233@gmail.com>
2025-03-18 09:43:46 +0800

    fix: update indexes

--- dephy-messaging-network/migrations/20250318002509_index.sql
+++ dephy-messaging-network/migrations/20250318002509_index.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY idx_session_mention_id ON events (session, mention, id);
-CREATE INDEX CONCURRENTLY idx_session_id ON events (session, id);
-CREATE INDEX CONCURRENTLY idx_mention_id ON events (mention, id);
-CREATE INDEX CONCURRENTLY idx_created_at_id ON events (created_at, id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_session_mention_id ON events (session, mention, id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_session_pubkey_id ON events (session, pubkey, id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_session_id ON events (session, id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_session_created_at_id ON events (session, created_at, id);
