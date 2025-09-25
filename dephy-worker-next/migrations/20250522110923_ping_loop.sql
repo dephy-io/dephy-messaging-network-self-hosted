10d3d09f8151d97b2aa5f791227cebcd25e1b63d
magine <magine233@gmail.com>
2025-09-25 23:13:14 +0800

    feat: update migration

--- dephy-worker-next/migrations/20250522110923_ping_loop.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE workers ADD COLUMN ping_loop_pinged BOOLEAN NOT NULL DEFAULT false;
--- dephy-worker-next/migrations/20250928085815_initial.sql
+++ dephy-worker-next/migrations/20250928085815_initial.sql
@@ -5,9 +5,29 @@ CREATE TABLE IF NOT EXISTS workers (
     status integer NOT NULL,
     created_at bigint NOT NULL,
     updated_at bigint NOT NULL,
+    ping_loop_pinged boolean NOT NULL,
+    "version" character varying NOT NULL,
+    backend character varying NOT NULL,
+    topic character varying NOT NULL,
 
     CONSTRAINT workers_unique_pubkey UNIQUE (pubkey)
 );
+
+CREATE TABLE IF NOT EXISTS worker_tasks (
+    id bigserial PRIMARY KEY,
+    worker_pubkey character varying NOT NULL,
+    status integer NOT NULL,
+    expires_at bigint NOT NULL,
+    request_event_id character varying NOT NULL,
+    result_event_id character varying,
+
+    CONSTRAINT worker_tasks_unique_request_event_id UNIQUE (request_event_id),
+
+    CONSTRAINT worker_tasks_fk_worker_pubkey
+    FOREIGN KEY (worker_pubkey) REFERENCES workers (pubkey)
+    ON DELETE CASCADE
+);
+
 
 CREATE TABLE IF NOT EXISTS online_proofs (
     id bigserial PRIMARY KEY,
