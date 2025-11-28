f51c982bdc7192f34e2171764488d6b1475bb5ac
magine <magine233@gmail.com>
2025-11-28 12:22:31 +0800

    feat: checkout diffs except dephy-worker-next

--- dephy-messaging-network/migrations/20250908102312_referenced_event.sql
+++ dephy-messaging-network/migrations/20250908102312_referenced_event.sql
@@ -1,2 +1,2 @@
 ALTER TABLE events ADD COLUMN referenced_event character varying;
-CREATE INDEX IF NOT EXISTS idx_session_mention_id ON events (session, referenced_event, id);
+CREATE INDEX IF NOT EXISTS idx_session_referenced_event_id ON events (session, referenced_event, id);
